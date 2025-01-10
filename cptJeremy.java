5dcfc3e7a7907ae355fd03a0bf9b76d4b27d6c26
jeremydylwong <jeremy.wong26@ycdsbk12.ca>
2025-01-09 22:48:49 -0500

    Completing deck array

--- cptJeremy.java
+++ cptJeremy.java
@@ -68,6 +68,62 @@ public class cptJeremy{
 		dblCards[50][0] = 12;
 		dblCards[51][0] = 13;
 		
+		dblCards[0][1] = 1;
+		dblCards[1][1] = 1;
+		dblCards[2][1] = 1;
+		dblCards[3][1] = 1;
+		dblCards[4][1] = 1;
+		dblCards[5][1] = 1;
+		dblCards[6][1] = 1;
+		dblCards[7][1] = 1;
+		dblCards[8][1] = 1;
+		dblCards[9][1] = 1;
+		dblCards[10][1] = 1;
+		dblCards[11][1] = 1;
+		dblCards[12][1] = 1;
+		
+		dblCards[13][1] = 2;
+		dblCards[14][1] = 2;
+		dblCards[15][1] = 2;
+		dblCards[16][1] = 2;
+		dblCards[17][1] = 2;
+		dblCards[18][1] = 2;
+		dblCards[19][1] = 2;
+		dblCards[20][1] = 2;
+		dblCards[21][1] = 2;
+		dblCards[22][1] = 2;
+		dblCards[23][1] = 2;
+		dblCards[24][1] = 2;
+		dblCards[25][1] = 2;
+		
+		dblCards[26][1] = 3;
+		dblCards[27][1] = 3;
+		dblCards[28][1] = 3;
+		dblCards[29][1] = 3;
+		dblCards[30][1] = 3;
+		dblCards[31][1] = 3;
+		dblCards[32][1] = 3;
+		dblCards[33][1] = 3;
+		dblCards[34][1] = 3;
+		dblCards[35][1] = 3;
+		dblCards[36][1] = 3;
+		dblCards[37][1] = 3;
+		dblCards[38][1] = 3;
+	
+		dblCards[39][1] = 4;
+		dblCards[40][1] = 4;
+		dblCards[41][1] = 4;
+		dblCards[42][1] = 4;
+		dblCards[43][1] = 4;
+		dblCards[44][1] = 4;
+		dblCards[45][1] = 4;
+		dblCards[46][1] = 4;
+		dblCards[47][1] = 4;
+		dblCards[48][1] = 4;
+		dblCards[49][1] = 4;
+		dblCards[50][1] = 4;
+		dblCards[51][1] = 4;
+		
 		
 	}
 	
